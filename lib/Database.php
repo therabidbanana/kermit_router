8e9294ebd0849fe61d8c14dd52f02e7af197a90c
David Haslem <haslem@gatech.edu>
2009-09-14 11:21:33 -0400

    Switching to use $this->core

--- lib/Database.php
+++ lib/Database.php
@@ -4,7 +4,7 @@ class Database extends Kermit_Module{
 	public function __construct($kermit){
 		$this->kermit = $kermit;
 		// Load Doctrine Core
-		$this->kermit->load('vendor/Doctrine');
+		$this->core->load('vendor/Doctrine');
 		// Register autoloader for Doctrine
 		spl_autoload_register(array('Doctrine', 'autoload'));
 	}
