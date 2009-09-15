b7b7dd24d18286a0ea91da82a609cdf22c76e11b
David Haslem <haslem@gatech.edu>
2009-09-15 11:04:19 -0400

    No need for aktrouter

--- bootstrap.php
+++ bootstrap.php
@@ -5,8 +5,7 @@
 define('__KERMIT_ROOT__', dirname(__FILE__));
 
 // Define config dir as KERMIT_ROOT
-define('__KERMIT_CONFIG__', dirname(__FILE__).'/config');
+define('__KERMIT_CONFIG__', dirname(__FILE__).'/'. 'config');
 
 // Load the loader
-require_once(__KERMIT_ROOT__ . '/lib/loader.php');
-Doctrine::generateModelsFromDb('models');
+require_once(__KERMIT_ROOT__ . '/'. 'lib' . '/'. 'loader.php');
